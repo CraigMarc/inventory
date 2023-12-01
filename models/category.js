a31c638ed836600a6296b9c45d7709ee139e3051
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-11-30 19:07:20 -0500

    added models

---
+++ models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, minLength: 1, maxLength: 100 },
+  description: { type: String, required: true },
+  
+});
+
+// Virtual for author's URL
+CategorySchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/category/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Category", CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, minLength: 1, maxLength: 100 },
+  description: { type: String, required: true },
+  price: { type: Integer, required: true },
+  number_in_stock: { type: Number, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+});
+
+// Virtual for author's URL
+ItemSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/item/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Item", ItemSchema);
