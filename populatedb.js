fb91cc9f64f576befa7e885930b4673f37536b4d
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-11-30 20:17:32 -0500

    added data to file

--- populatedb.js
+++ populatedb.js
@@ -59,7 +59,7 @@ console.log(
     console.log(`Added author: ${first_name} ${family_name}`);
   }*/
 
-  async function CategoryCreate(index, name, description) {
+  async function categoryCreate(index, name, description) {
     const itemdetail = {
       name: name,
       description: description,
@@ -74,7 +74,7 @@ console.log(
   }
 
   
-  async function ItemCreate(index, name, description, price, number, category) {
+  async function itemCreate(index, name, description, price, number, category) {
     const itemdetail = {
       name: name,
       description: description,
@@ -112,69 +112,67 @@ console.log(
       genreCreate(2, "French Poetry"),
     ]);
   }*/
-  /*
-  async function createAuthors() {
-    console.log("Adding authors");
-    await Promise.all([
-      authorCreate(0, "Patrick", "Rothfuss", "1973-06-06", false),
-      authorCreate(1, "Ben", "Bova", "1932-11-8", false),
-      authorCreate(2, "Isaac", "Asimov", "1920-01-02", "1992-04-06"),
-      authorCreate(3, "Bob", "Billings", false, false),
-      authorCreate(4, "Jim", "Jones", "1971-12-16", false),
-    ]);
-  }*/
   
-  async function createBooks() {
-    console.log("Adding Books");
+  async function createCategory() {
+    console.log("Adding Categories");
     await Promise.all([
-      bookCreate(0,
-        "The Name of the Wind (The Kingkiller Chronicle, #1)",
-        "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
-        "9781473211896",
-        authors[0],
-        [genres[0]]
+      categoryCreate(0, "Bikes", "full bikes", "1973-06-06"),
+      categoryCreate(1, "Parts", "bike parts", "1932-11-8"),
+      categoryCreate(2, "Accessories", "bike accessories"),
+    ]);
+  }
+  
+  async function createItems() {
+    console.log("Adding Items");
+    await Promise.all([
+      itemCreate(0,
+        "Trek Fuel EX 5 Deore Gen 5",
+        "Fuel EX 5 is the gateway to full suspension trail bikes. Trail-taming front and rear suspension, a Shimano 1x12 drivetrain, a dropper post for getting low on descents, and hydraulic disc brakes make Fuel EX 5 a high-value, high-performance mountain bike with exceptional versatility.",
+        2,099.99,
+        6,
+        [categories[0]]
       ),
-      bookCreate(1,
-        "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-        "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
-        "9788401352836",
-        authors[0],
-        [genres[0]]
+      itemCreate(1,
+        "Trek Roscoe 7",
+        "Roscoe 7 is a hardtail for riders who wanna have a blast ripping up the trail. A plush 140mm suspension fork, 29er wheels, and a wide-range drivetrain with plenty of gears make this bike great for new riders yet playful enough to make even the most experienced shredders smile.",
+        1,349.99,
+        5,
+        [categories[0]]
       ),
-      bookCreate(2,
-        "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-        "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
-        "9780756411336",
-        authors[0],
-        [genres[0]]
+      itemCreate(2,
+        "Shimano GRX RX400 Hydraulic Disc Brake",
+        "With a low-profile Flat Mount design, the Shimano GRX BR-RX400 hydraulic disc brakes provide greater control for mixed terrain riding.",
+        54.99,
+        10,
+        [categories[1]]
       ),
-      bookCreate(3,
-        "Apes and Angels",
-        "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
-        "9780765379528",
-        authors[1],
-        [genres[1]]
+      itemCreate(3,
+        "Shimano GRX RX812 11-Speed Rear Derailleur",
+        "With an integrated chain stabilizer mechanism, the Shimano GRX derailleur provides a quieter experience with shifting security and precision on mixed terrain. For rides on smooth surfaces, the derailleur features and ON/OFF switch for the chain stabilizer. The RD-RX812 rear derailleur is compatible with MTB cassettes with a max cog size of 42T.",
+        119.99,
+        6,
+        [categories[1]]
       ),
-      bookCreate(4,
-        "Death Wave",
-        "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-        "9780765379504",
-        authors[1],
-        [genres[1]]
+      itemCreate(4,
+        "RockShox Deluxe Ultimate Rear Shock",
+        "A perfect match for the current crop of ultra-lightweight and capable dream bikes, Deluxe Ultimate offers a fully checked damper design allowing for a Low Speed Compression adjustment and optimal traction control tuning. It also features Maxima Plush fluid for added performance and ride quality so you can go on and shred to your heart’s content.",
+        407.00,
+        3,
+        [categories[1]]
       ),
-      bookCreate(5,
-        "Test Book 1",
-        "Summary of test book 1",
-        "ISBN111111",
-        authors[4],
-        [genres[0], genres[1]]
+      itemCreate(5,
+        "Cannondale Dirt Mini Pump",
+        "Stay pumped. Packed with clever features that prioritize convenience and functionality, and gets large volume off-road tires to the correct pressures quickly.",
+        48.00,
+        6,
+        [categories[3]]
       ),
-      bookCreate(6,
-        "Test Book 2",
-        "Summary of test book 2",
-        "ISBN222222",
-        authors[4],
-        false
+      itemCreate(6,
+        "Bontrager Bontrager Circuit Twin Gel Cycling Glove",
+        "A versatile bike glove with optimized gel and gel foam padding for all-day comfort.",
+        15.99,
+        12,
+        [categories[3]]
       ),
     ]);
   }
