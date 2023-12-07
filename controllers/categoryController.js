ac9b5f6b4ddc55f93bbda075d7d9631306106221
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-07 12:24:48 -0500

    added category form

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,7 @@
 const Category = require("../models/category");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
-
+const { body, validationResult } = require("express-validator");
 
 //homepage
 exports.index = asyncHandler(async (req, res, next) => {
@@ -47,14 +47,80 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 
 
 // Display Category create form on GET.
+/*
 exports.category_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category create GET");
-});
+{);*/
+  exports.category_create_get = (req, res, next) => {
+    res.render("category_form", { title: "Create Category" });
+  };
+
+
 
 // Handle Category create on POST.
+/*
 exports.category_create_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category create POST");
-});
+});*/
+
+exports.category_create_post = [
+  // Validate and sanitize the name field.
+  /*
+  body("name", "Category name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),*/
+
+    body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Category name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Category name has non-alphanumeric characters."),
+  body("description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Description must be specified.")
+    .isAlphanumeric()
+    .withMessage("Description has non-alphanumeric characters."),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a category object with escaped and trimmed data.
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      const categoryExists = await Category.findOne({ name: req.body.name }).exec();
+      if (categoryExists) {
+        // Category exists, redirect to its detail page.
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+        // New category saved. Redirect to category detail page.
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
 
 // Display Category delete form on GET.
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
